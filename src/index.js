1cac804323a3e5e74b3669f82de4c7e87c3b34a6
luzhongkuan <luzhongk@126.com>
2018-07-20 13:26:00 +0800

    去除console.log

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import Vue from 'vue'
 
 import App from './App'
 
-import waterFall from './waterFall'
-console.log(waterFall)
-Vue.use(waterFall) // 全局注册
+// import waterFall from './waterFall'
+// Vue.use(waterFall) // 全局注册
 
 Vue.config.productionTip = false
 
