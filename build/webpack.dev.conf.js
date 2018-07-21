956e25d5bdc8488e9989f0eecb039e4ff5340b66
luzhongkuan <zhongkuan.lu@vhuomai.com>
2018-07-21 12:04:03 +0800

    portfinder

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -3,14 +3,19 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const merge = require('webpack-merge');
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+const portfinder = require('portfinder')
+const ip = require('ip')
 
 const common = require('./webpack.base.conf.js');
+
+const PORT = 8088
 
 function resolve(dir) {
   return path.join(__dirname, '../', dir);
 }
 
-module.exports = merge(common, {
+const webpackConfig = merge(common, {
   mode: 'development',
   plugins: [
     new CopyWebpackPlugin([
@@ -39,7 +44,7 @@ module.exports = merge(common, {
     // hot: true,
     // inline: true,
     // contentBase: [path.join(__dirname, '../public'),],
-    port: process.env.PORT || 8088,
+    port: process.env.PORT || PORT,
     publicPath: '/',
     host: '0.0.0.0',
     openPage: '',
@@ -50,3 +55,23 @@ module.exports = merge(common, {
     open: true
   },
 });
+
+module.exports = new Promise((resolve, reject) => {
+  portfinder.basePort = process.env.PORT || PORT
+  portfinder.getPort((err, port) => {
+    if (err) {
+      reject(err)
+    } else {
+      process.env.PORT = port
+      webpackConfig.devServer.port = port
+
+      webpackConfig.plugins.push(new FriendlyErrorsPlugin({
+        compilationSuccessInfo: {
+          messages: [`Your application is running here: http://${ip.address()}:${port}`],
+        }
+      }))
+
+      resolve(webpackConfig)
+    }
+  })
+})
