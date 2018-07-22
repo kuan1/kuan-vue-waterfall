190cb45bca684bf4e5689998de4153da171229ab
luzhongkuan <zhongkuan.lu@vhuomai.com>
2018-07-22 12:34:20 +0800

    ..

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const base = require('./webpack.base.conf')
 const merge = require('webpack-merge')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 const config = merge(base, {
   context: path.resolve(__dirname, '../'),
@@ -12,6 +13,7 @@ const config = merge(base, {
     libraryTarget: 'umd'
   },
   plugins: [
+    new CleanWebpackPlugin(['lib'], {root: path.resolve(__dirname, '..')})
     // ,
     // new webpack.DefinePlugin({
     //   'process.env': {
