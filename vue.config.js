03160c8e2e7bc3f2184f23bb4e6432634b469c72
luzhongkuan <luzhongk@126.com>
2020-11-22 12:48:21 +0800

    chore: 打包

--- vue.config.js
+++ vue.config.js
@@ -5,14 +5,28 @@ const isDev = process.env.NODE_ENV === 'development'
 const resolve = (dir) => path.resolve(__dirname, dir)
 
 const distName = 'demo' // 项目名称
+const testEntry = path.resolve(__dirname, 'test/index.js')
+const srcEntry = path.resolve(__dirname, 'src/index.js')
 
-const config = {
+const demoConfig = {
   publicPath: isDev ? '/' : `/kuan-vue-waterfall/demo/`, // public path
   outputDir: resolve(`${distName}`), // 输入地址
   devServer: {
     proxy: 'https://www.luzhongkuan.cn',
     disableHostCheck: true,
   },
+  configureWebpack: {
+    entry: {
+      app: testEntry,
+    },
+  },
+  chainWebpack(config) {
+    config.resolve.alias.set('kuan-vue-waterfall', path.resolve(__dirname, 'src'))
+  }
+}
+
+const libConfig = {
+  outputDir: resolve(`${distName}`), // 输入地址
   css: {
     extract: false,
   },
@@ -21,7 +35,7 @@ const config = {
       vue: { commonjs: 'vue', commonjs2: 'vue', root: 'Vue' },
     },
     entry: {
-      app: path.resolve(__dirname, 'src/index.js'),
+      app: srcEntry,
     },
   },
 }
@@ -30,7 +44,7 @@ const testConfig = {
   publicPath: '/', // public path
   configureWebpack: {
     entry: {
-      app: path.resolve(__dirname, 'test/index.js'),
+      app: testEntry,
     },
   },
   chainWebpack(config) {
@@ -38,4 +52,11 @@ const testConfig = {
   }
 }
 
-module.exports = process.env.BUILD_TARGET === 'test' ? testConfig : config
+const configMap = {
+  test: testConfig,
+  demo: demoConfig
+}
+
+const config = configMap[process.env.BUILD_TARGET] || libConfig
+
+module.exports = config
